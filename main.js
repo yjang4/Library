fd9b8a408f103e84eb474282a1809432ea001248
Daniel <mikyjo2012@gmail.com>
2022-07-22 08:01:01 -0500

    can add books

---
+++ main.js
@@ -0,0 +1,33 @@
+let myLibrary = [];
+
+function Book(title) {
+    this.title = title;
+}
+
+function addBookToLibrary(book) {
+  // do stuff here
+  myLibrary.push(book);
+}
+
+/*
+const container = document.getElementById("container");
+let card = document.createElement("div");
+card.className = "card";
+card.textContent = myBook.title;
+container.appendChild(card);*/
+
+const btnSubmit = document.getElementById("btn-submit");
+const inputTitle = document.getElementById("input-title");
+btnSubmit.addEventListener("click", function() {
+  let myBook = new Book(inputTitle.value);
+  addBookToLibrary(myBook);
+  let card = document.createElement("div");
+  card.className = "card";
+  card.textContent = myBook.title;
+  let readBtn = document.createElement("button");
+  readBtn.textContent = "mark read";
+  let deleteBtn = document.createElement("button");
+  readBtn.textContent = "X";
+  card.appendChild(readBtn);
+  container.appendChild(card);
+});
